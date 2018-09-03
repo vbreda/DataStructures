f58fb76ab1e81a39e370ac9e073e5c35e3398260
joking <joking@oucs1352.otago.ac.nz>
2018-09-04 10:50:37 +1200

    minor changes

--- tree.h
+++ tree.h
@@ -10,7 +10,6 @@ typedef enum tree_e { BST, RBT } tree_t;
 extern tree right_rotate(tree t);
 extern tree left_rotate(tree t);
 extern tree tree_fix(tree t);
-extern tree tree_delete(tree t, char *str);
 extern tree tree_free(tree t);
 extern void tree_inorder(tree t, void f(int freq, char *str));
 extern tree tree_insert(tree t, char *str);
