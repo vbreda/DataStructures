f73e64abf3b54f3f38b1874b7952e741deafb9c4
Vivian Breda <vivianbreda@gmail.com>
2018-09-06 09:42:58 +1200

    Add supllied code to htable.c and update htable.h to reflect the changes

--- htable.h
+++ htable.h
@@ -13,5 +13,5 @@ extern htable htable_new(int capacity, hashing_t method);
 extern void htable_print(htable h, void f(int freq, char *str));
 extern void htable_print_entire_table(htable h, FILE *stream);
 extern int htable_search(htable h, char *str);
-
+extern void htable_print_stats(htable h, FILE *stream, int num_stats);
 #endif
