7cea007eda61fddd3a1e34169f9c86b128bce150
Vivian Breda <vivianbreda@gmail.com>
2018-09-05 16:02:39 +1200

    Add hash table code to the project

---
+++ hash_table/hashsearch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mylib.h"
+#include "htable.h"
+
+int main(void) {
+    
+    htable h = htable_new(113);
+    char word[256];
+    char op;
+    
+    /* We must have a space before the %c */
+    while (2 == scanf(" %c %255s", &op, word)) {
+        if ('+' == op) {
+            htable_insert(h, word);
+        } else if ('?' == op) {
+            printf("%d %s\n", htable_search(h, word), word);
+        }
+    }
+    htable_free(h);
+    return EXIT_SUCCESS;
+}
