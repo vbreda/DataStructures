3b80845647c2c873267cf3c516aa815e9b66ab1f
asaravanapavan <asaravanapavan@oucs1458.otago.ac.nz>
2018-09-10 14:47:04 +1200

    All comments added

--- mylib.c
+++ mylib.c
@@ -1,9 +1,21 @@
+/**
+ * File: htable.c
+ * @author: Vivian Breda, Josh King, Abinaya Saravanapavan.
+ */
+
 #include <stdio.h> /* for fprintf */
 #include <stdlib.h>/* for size-t, malloc, realloc, exit */
 #include <assert.h> /* for assert*/
 #include <ctype.h>
 #include "mylib.h"
 
+/**
+ * Function: emalloc
+ * Purpose: allocates a block of memory.
+ *
+ * @param s the size ( bytes) of memory required for allocation.
+ * @return the memory address where it has been allocated.
+ */
 void *emalloc(size_t s){
     void *result = malloc(s);
     if(NULL == result){
@@ -13,6 +25,14 @@ void *emalloc(size_t s){
     return result;
 }
 
+/**
+ * Function: erealloc
+ * Purpose: reallocates a block of memory.
+ *
+ * @param p is a void pointer to reallocate memory.
+ * @param s the size (bytes) of memory required for reallocation.
+ * @return the memory address where it has been reallocated.
+ */
 void *erealloc(void *p, size_t s){
     void *result = realloc(p, s);
     if (NULL == result){
@@ -22,18 +42,26 @@ void *erealloc(void *p, size_t s){
     return result;
 }
 
+/**
+ * Function: getword
+ * Purpose: reads in the stream which is the file passed. 
+ *
+ * @param s the char pointer to the array s.
+ * @param limit the array size of s.
+ * @param stream the source of the input.
+ * @return -1 when end of file reached, or the word that has been read in.  
+ */
 int getword(char *s, int limit, FILE *stream)
 {
     int c;
     char *w = s;
     assert(limit > 0 && s!= NULL && stream != NULL);
 
-    /*Skip to the start of the word*/
     while(!isalnum(c = getc(stream)) && EOF != c)
         ;
     if(EOF == c ){
         return EOF;
-    }else if(--limit > 0){/*reduce limit by 1 to allow for the \0*/
+    }else if(--limit > 0){
         *w++ = tolower(c);
     }
     while(--limit > 0){
