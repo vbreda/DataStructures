7e4f5970a6703b7dd228112715a23eeacea36443
asaravanapavan <asaravanapavan@oucs1458.otago.ac.nz>
2018-09-10 14:31:26 +1200

    All comments added but may need comments for each function in this header?

--- htable.h
+++ htable.h
@@ -1,12 +1,26 @@
+/**
+ *  File: htable.h
+ * @author Vivian Breda, Josh King, Abinaya Saravanapavan.
+ */
+
 #ifndef HTABLE_H_
 #define HTABLE_H_
 
 #include <stdio.h>
 
+/**
+ * Struct: htablerec
+ * Purpose: defining a struct type of htablerec to structure the htable.
+ */
 typedef struct htablerec *htable;
 
 typedef enum hashing_e { LINEAR_P, DOUBLE_H } hashing_t;
 
+/**
+ * Prototypes
+ * Purpose: specifies functions to be implemented in the htable.c file, based on
+ * their signatures.
+ */
 extern void htable_free(htable h);
 extern int htable_insert(htable h, char *str);
 extern htable htable_new(int capacity, hashing_t method);
